88e3e3dcaf32021142456e9a41a64aa1e17e5dbb
khnychkin_kirill <kirill.khnychkin@gmail.com>
2025-05-20 17:00:10 +0400

    Update worktree scripts to reference the correct aider configuration file path

--- git/worktree_add.sh
+++ git/worktree_add.sh
@@ -19,7 +19,7 @@ mkdir -p ./.cursor/rules
 cp -a /Users/user/Documents/Settings/ide/vs_code,cursor/_cursor_general_project_rules/ ./.cursor/rules/ # universal rules
 cp -a ../.project_cursorrules/ ./.cursor/rules/ # project specific rules
 
-cp -a /Users/user/Documents/Settings/ide/vs_code,cursor/.aider.conf.yml ./ # aider rules
+cp -a ../.aider.conf.yml ./ # aider rules
 
 DIRNAME=$(dirname "$0")
 $DIRNAME/../ide/launch_current_ide_in_pwd.sh
--- git/worktree_add_without_ide.sh
+++ git/worktree_add_without_ide.sh
@@ -19,4 +19,4 @@ mkdir -p ./.cursor/rules
 cp -a /Users/user/Documents/Settings/ide/vs_code,cursor/_cursor_general_project_rules/ ./.cursor/rules/ # universal rules
 cp -a ../.project_cursorrules/ ./.cursor/rules/ # project specific rules
 
-cp -a /Users/user/Documents/Settings/ide/vs_code,cursor/.aider.conf.yml ./ # aider rules
+cp -a ../.aider.conf.yml ./ # aider rules
