14ad6f08fa237ae1c63ebe794ed7e586ffd99c31
khnychkin_kirill <kirill.khnychkin@gmail.com>
2025-05-28 17:33:41 +0400

    Refactor cursor rules handling in worktree scripts and update .gitignore for clarity

--- git/worktree_add_without_deps.sh
+++ git/worktree_add_without_deps.sh
@@ -6,9 +6,10 @@ cd ../$1
 cp ../.env.auth ./.env.auth
 cp ../.env.local ./.env.local
 
-mkdir -p ./.cursor/rules
-cp -a /Users/user/Documents/Settings/ide/vs_code,cursor/cursor_general_project_rules/ ./.cursor/rules/ # universal rules
-cp -a ../.project_cursorrules/ ./.cursor/rules/ # project specific rules
+# Cursor rules are under git from 28.05.2025
+# mkdir -p ./.cursor/rules
+# cp -a /Users/user/Documents/Settings/ide/vs_code,cursor/cursor_general_project_rules/ ./.cursor/rules/ # universal rules
+# cp -a ../.project_cursorrules/ ./.cursor/rules/ # project specific rules
 
 cp -a ../.aider.conf.yml ./ # aider rules
 
