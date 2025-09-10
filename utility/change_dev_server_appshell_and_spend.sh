9a0971d92767b502f5774e365ee78ff6323f0bd0
Global User <global@example.com>
2025-09-11 03:22:54 +0400

    Update change_dev_server_appshell_and_spend.sh to modify paths for environment files in the spend project

--- utility/change_dev_server_appshell_and_spend.sh
+++ utility/change_dev_server_appshell_and_spend.sh
@@ -4,15 +4,15 @@
 
 if [ -z "$1" ]
     then
-       printf "Enter the server number\n"
-       read serverNumber
+      printf "Enter the server number\n"
+      read serverNumber
 
-       sed -i '' s/dev\[0-9]\/dev$serverNumber/g /Users/user/Mysky/projects/mysky_app_shell/.env.local
-       sed -i '' s/dev\[0-9]\/dev$serverNumber/g /Users/user/Mysky/projects/mysky_spend/.env.local
-       printf "\nDev server URLs in App-Shell and Spend changed to $serverNumber\n\n"
+      sed -i '' s/dev\[0-9]\/dev$serverNumber/g /Users/user/Mysky/projects/mysky_app_shell/.env.local
+      sed -i '' s/dev\[0-9]\/dev$serverNumber/g /Users/user/Mysky/projects/_mysky_spend/master/.env.local
+      printf "\nDev server URLs in App-Shell and Spend changed to $serverNumber\n\n"
     else
       sed -i '' s/dev\[0-9]\/dev$1/g /Users/user/Mysky/projects/mysky_app_shell/.env.local
-      sed -i '' s/dev\[0-9]\/dev$1/g /Users/user/Mysky/projects/mysky_spend/.env.local
+      sed -i '' s/dev\[0-9]\/dev$1/g /Users/user/Mysky/projects/_mysky_spend/master/.env.local
       printf "\nDev server in App-Shell and Spend changed to $1\n\n"
     fi
 
